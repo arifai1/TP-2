5a096d7553c14c2ca60ec9c5a0c4d118dd3b2eca
ari <arifaiwu@gmail.com>
2022-04-04 12:07:34 -0300

    Created excersices 1 and 2. Finish excersice 1. Started Excersice 2

---
+++ Assets/MayoriaDeEdad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int edadUsuario;
+    public Text Mi_texto;
+
+    void Start()
+    {
+       if(edadUsuario >= 18)
+        {
+            Mi_texto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Mi_texto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
