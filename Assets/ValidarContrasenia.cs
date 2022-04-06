4806e26ed566026ad960f979dd48344ef48b0e17
ari <arifaiwu@gmail.com>
2022-04-06 11:53:04 -0300

    Updated with video 6.
    Last: created cartelitomsj.SetActive(false);

---
+++ Assets/ValidarContrasenia.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresousuario;
+    public GameObject cartelitomsj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+        cartelitomsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniaUsuario = ingresousuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+
+}
